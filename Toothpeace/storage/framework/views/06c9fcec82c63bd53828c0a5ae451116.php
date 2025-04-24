7ec1b5a10d588a20a553e074034b6ab5639fb8c2
jhonlynx <jpaulnoquiana2@gmail.com>
2025-04-24 17:19:09 +0800

    Add files via upload

---
+++ Toothpeace/storage/framework/views/06c9fcec82c63bd53828c0a5ae451116.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Discover Peace of Mind, One Appointment at a Time.">
+
+    <meta property="og:title" content="ToothPeace">
+    <meta property="og:description" content="Discover Peace of Mind, One Appointment at a Time.">
+    <meta property="og:image" content="https://example.com/image.jpg">
+    <meta property="og:url" content="https://example.com">
+
+    <link rel="icon" href="<?php echo e(asset('pics/toothpeace_logo.png')); ?>" type="image/x-icon">
+    <link rel="stylesheet" href="<?php echo e(asset('css/landing.css')); ?>">
+    <script src="<?php echo e(asset('js/jslanding.js')); ?>"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <title>ToothPeace</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="<?php echo e(asset('pics/toothpeace_logo.png')); ?>" alt="ToothPeace Logo" />
+        </div>
+    </header>
+
+    <section class="landing_page">
+        <h1>
+            <span class="blue">TOOTH</span>
+            <span class="skyblue">PEACE</span>
+        </h1>
+        <p>Discover Peace of Mind, One Appointment at a Time.</p>
+        <div class="buttons">
+            <button onclick="location.href='login'">Proceed to Login</button> <br>
+            <button onclick="location.href='services'">View Services</button>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2025 ToothPeace Dental Clinic</p>
+    </footer>
+</body>
+</html>
+<?php /**PATH C:\Users\Jhon Paul\Downloads\ToothPeace\ToothPeace-master\resources\views/index.blade.php ENDPATH**/ ?>
